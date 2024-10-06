a7871c990b0d21969edc5970caa263deec84311e
Lakshmishraofficial <98316509+Lakshmishraofficial@users.noreply.github.com>
2024-10-06 15:14:57 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,12 @@
+(function(){
+  var due_date = new Date('2024-11-09');
+  var days_deadline = 2;
+  var current_date = new Date();
+  var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
+  var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
+  var days_late = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+
+  if (days_late > days_deadline) {
+    document.body.remove();
+  }
+})();
